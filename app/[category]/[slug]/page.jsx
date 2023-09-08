23d3947ce5b6ddccac399e66890d54d42f19f591
zonggonau <cristoperzonggonau@gmail.com>
2023-09-08 21:00:15 +0900

    upgrade

--- app/[category]/[slug]/page.jsx
+++ app/[category]/[slug]/page.jsx
@@ -1,12 +1,9 @@
-"use client";
 import React from "react";
 
 import CompDetailTour from "@/components/detailstour";
 import { getDataDetails } from "@/data/api";
-import { useParams } from "next/navigation";
 
-export default async function Details() {
-  const params = useParams();
+export default async function Details({ params }) {
   const category = params.category;
   const slug = params.slug;
   console.log(category);
