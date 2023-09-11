98be1dbb417aac4d1d39c44208030d2f31aa0a12
zonggonau <cristoperzonggonau@gmail.com>
2023-09-11 09:31:09 +0900

    upgrade

--- app/tour-adventures/page.jsx
+++ app/tour-adventures/page.jsx
@@ -1,5 +1,4 @@
-"use client";
-import React, { useState } from "react";
+import React,  from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { dataCarouselHero } from "@/data";
