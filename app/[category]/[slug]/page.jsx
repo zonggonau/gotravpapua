65bd47d99d0bfa4332f5a30f4fbca0fbf3bb8693
zonggonau <cristoperzonggonau@gmail.com>
2023-09-13 20:32:13 +0900

    upgrade seo

--- app/[category]/[slug]/page.jsx
+++ app/[category]/[slug]/page.jsx
@@ -3,17 +3,79 @@ import React from "react";
 import CompDetailTour from "@/components/detailstour";
 import { getDataDetails } from "@/data/api";
 
-export const metadata = {
-  title: "GoTravPapua Tour Details",
-  description:
-    "Get All the Tour Details You Need for Your SEO-Enhanced Adventure. Plan Your Trip with Confidence!",
-};
+import { dataCarouselHero } from "@/data";
+export async function generateMetadata({ params }) {
+  const category = params.category;
+  const slug = params.slug;
+  const { data } = await getDataDetails(category, slug);
+  return {
+    title: "GoTravPapua | " + data.title,
+    openGraph: {
+      title: "GoTravPapua | " + data.title,
+      description: data.title,
+      url: process.env.NEXT_PUBLIC_HOST,
+      siteName: "GoTravPapua",
+      images: [
+        {
+          url: process.env.NEXT_PUBLIC_HOST + data.picture,
+          width: 800,
+          height: 600,
+        },
+        {
+          url: process.env.NEXT_PUBLIC_HOST + data.picture,
+          width: 1800,
+          height: 1600,
+        },
+        {
+          url: process.env.NEXT_PUBLIC_HOST + data.picture,
+          width: 1080,
+          height: 675,
+        },
+      ],
+      locale: "en_US",
+      type: "website",
+    },
+    robots: {
+      index: false,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: false,
+        noimageindex: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "GoTravPapua | " + data.title,
+      description: data.description,
+      images: [process.env.NEXT_PUBLIC_HOST + data.picture],
+    },
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      maximumScale: 1,
+    },
+    verification: {
+      google: "google",
+      yandex: "yandex",
+      yahoo: "yahoo",
+      other: {
+        me: [
+          "admin@gotravpapua.com",
+          process.env.NEXT_PUBLIC_HOSTNAME + "tour-contact",
+        ],
+      },
+    },
+  };
+}
 
 export default async function Details({ params }) {
   const category = params.category;
   const slug = params.slug;
-  console.log(category);
-  console.log(slug);
   const { data } = await getDataDetails(category, slug);
   return <CompDetailTour data={data} />;
 }
